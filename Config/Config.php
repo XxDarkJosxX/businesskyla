735d10a4b2d0c206c1ca7e737bca26d5673cc127
JoseM <maytadazajos@gmail.com>
2025-04-16 14:06:07 -0400

    v12

--- Config/Config.php
+++ Config/Config.php
@@ -3,22 +3,22 @@
    date_default_timezone_set('America/La_Paz');
 
    
-    // const BASE_URL = "https://nathanpublicidad.com/businesskyla";
-    // const DB_HOST = "localhost";
-    // const DB_NAME = "";
-    // const DB_USER = "root";
-    // const DB_PASSWORD = "";
-
-    // const DB_CHARSET = "utf8";
-
-
-    const BASE_URL = "http://localhost/businesskyla";
+    const BASE_URL = "https://nathanpublicidad.com/businesskyla";
     const DB_HOST = "localhost";
     const DB_NAME = "";
     const DB_USER = "root";
     const DB_PASSWORD = "";
 
     const DB_CHARSET = "utf8";
+
+
+    // const BASE_URL = "http://localhost/businesskyla";
+    // const DB_HOST = "localhost";
+    // const DB_NAME = "";
+    // const DB_USER = "root";
+    // const DB_PASSWORD = "";
+
+    // const DB_CHARSET = "utf8";
 
 
 
